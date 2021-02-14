8b372323591c10c3e308e9cfc9af1d66b1ef404c
Daniel Yuan <dyuan@gatech.edu>
2021-02-14 09:55:55 -0500

    Implement first POST test function, successful test.

--- functions/index.js
+++ functions/index.js
@@ -14,8 +14,35 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 
+// Service account
+var serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialcompetitionapp..firebaseio.com"
+});
+const db = admin.firestore();
+
+// BEGINNING OF FUNCTIONS
+
 app.get('/hello-world', (req, res) => {
   return res.status(200).send('Hello World!');
 });
+
+// create
+app.post('/api/create', (req, res) => {
+    (async () => {
+        const body = JSON.parse(req.body);
+
+        console.log('id parsed', body.id);
+        try {
+          await db.collection('comments').doc('/' + body.id + '/')
+              .create({item: body.item});
+          return res.status(200).send();
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+      })();
+  });
 
 exports.app = functions.https.onRequest(app);
